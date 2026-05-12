bf62bb0d4d10b6277a96358e34ca5c7818d61d5f
Laura <lmartinj05@educarex.es>
2026-05-12 10:35:08 +0200

    Configurado de proyecto2

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -29,7 +29,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'DocenteDigital', // Usually your GitHub org/user name.
   projectName: 'ServidorWeb', // Usually your repo name.
-  deploymentBranch: 'gh-page',
+  deploymentBranch: 'gh-pages',
   trailingSlash: false,
   onBrokenLinks: 'throw',
 
