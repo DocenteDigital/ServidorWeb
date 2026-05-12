0de6e1c49ca5b7ccba89aabf300696bf1304bff3
Laura <lmartinj05@educarex.es>
2026-05-12 10:39:07 +0200

    Configurado de proyecto2

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://github.com/DocenteDigital',
+  url: 'https://DocenteDigital.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/ServidorWeb/',
@@ -29,7 +29,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'DocenteDigital', // Usually your GitHub org/user name.
   projectName: 'ServidorWeb', // Usually your repo name.
-  deploymentBranch: 'gh-pages',
+  deploymentBranch: 'introduccion-doc',
   trailingSlash: false,
   onBrokenLinks: 'throw',
 
